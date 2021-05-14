f58b36de1061cb5805e686fb10c706fad4442919
Jon <jon9544@gmail.com>
2021-05-14 13:31:39 -0500

    Update get-ssl.sh
    updated stop/start commands

--- get-ssl.sh
+++ get-ssl.sh
@@ -4,11 +4,11 @@
 
 # Change the domain variable to the domain/subdomain for which you would like
 # an SSL Certificate
-DOMAIN="fms.swcarrier.com"
+DOMAIN="fms.domain.com"
 
 # Change the contact email address to your real email address so that Let's Encrypt
 # can contact you if there are any problems #>
-EMAIL="jon@swcarrier.com"
+EMAIL="email@domain.com"
 
 # Enter the path to your FileMaker Server directory, ending in a slash 
 SERVER_PATH="/opt/FileMaker/FileMaker Server/"
@@ -40,3 +40,9 @@ fmsadmin certificate delete --yes -u ${FMADMIN} -p ${FMPASS}
 
 # Install the certificate
 fmsadmin certificate import "${SERVER_PATH}CStore/fullchain.pem" --keyfile "${SERVER_PATH}CStore/privkey.pem" -y -u ${FMADMIN} -p ${FMPASS}
+
+# Stop FM service
+sudo service fmshelper stop
+
+# Start FM service
+sudo service fmshelper start
